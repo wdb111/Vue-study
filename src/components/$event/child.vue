da780ebc28e6bf2dcbc78f6c8243bea7819199df
wdb111 <1303329454@qq.com>
2021-02-24 18:25:46 +0800

    2.24

--- src/components/$event/child.vue
+++ src/components/$event/child.vue
@@ -1,25 +1,43 @@
 <template>
-  <div>
-    <p>我是子组件</p>
-    <el-button @click="getVal">调用父组件函数</el-button>
-  </div>
+    <div class="bbb">
+      <div class="ccc">
+ <p>
+            我是子组件中的随机颜色：{{ msg }}
+            <el-color-picker v-model="msg"></el-color-picker>
+        </p>
+        <el-button @click="getVal">调用父组件函数</el-button>
+      </div>
+       
+    </div>
 </template>
 
 <script>
 export default {
-  name: "demo",
-  data() {
-    return {};
-  },
-  computed: {},
-  methods: {
-      getVal(){
-          this.$emit("myEvent","子组件传递的参数")
-      }
-  },
-  created() {}
+    name: "demo",
+    data() {
+        return {
+            msg: 0,
+        };
+    },
+    computed: {},
+    methods: {
+        getVal() {
+            this.$emit("myEvent", "子组件传递的参数");
+        },
+    },
+    created() {
+        let str = Math.random().toString(16).substr(-6);
+        this.msg = '#'+str;
+        console.log("created");
+    },
+    mounted() {
+        console.log("mounted");
+    },
 };
 </script> 
 
-<style lang='less' scoped>
+<style scoped>
+.ccc {
+  height: 100%;
+}
 </style>
--- src/components/$event/index.vue
+++ src/components/$event/index.vue
@@ -1,45 +1,54 @@
 <template>
-  <div>
-    <h3>vue中<span style="color:red">$event</span>的两个作用：</h3>
-    <p>1.在原生事件中表现和默认的事件对象相同</p>
-    <input type="text" @input="inputHandler('hello', $event)" />
-    <p>{{msg}}</p>
-    <p>2.在自定义事件中表现为捕获从子组件抛出的值</p>
-    <Child @myEvent="getValue('123',$event)"></Child>
-    <p>{{msg1}}</p>
-  </div>
+    <div>
+        <h3>vue中<span style="color: red">$event</span>的两个作用：</h3>
+        <p>1.在原生事件中表现和默认的事件对象相同</p>
+        <input type="text" @input="inputHandler('hello', $event)" />
+        <p>{{ msg }}</p>
+        <p>2.在自定义事件中表现为捕获从子组件抛出的值</p>
+        <Child class="aaa" @myEvent="getValue('123', $event)" :key="timer"></Child>
+        <p>{{ msg1 }}</p>
+        <el-button @click="refresh">通过key刷新组件</el-button>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "demo",
-  props: {},
-  components: {
-    Child: () => import("./child")
-  },
-  data() {
-    return {
-      msg: "000",
-      msg1:""
-    };
-  },
-  computed: {},
-  methods: {
-    inputHandler(val, e) {
-      // e.target 是你当前点击的元素
-      // e.currentTarget 是你绑定事件的元素
-      console.log(val, e.target);
-      this.msg = e.target.value;
+    name: "demo",
+    props: {},
+    components: {
+        Child: () => import("./child"),
     },
-    getValue(val, e) {
-        //如果父组件中不传参，则第一个参数为子组件抛出的参数
-      console.log(val, e);
-      this.msg1=e
-    }
-  },
-  created() {}
+    data() {
+        return {
+            timer: 0,
+            msg: "000",
+            msg1: "",
+        };
+    },
+    computed: {},
+    methods: {
+        refresh() {
+            this.timer = Date.now();
+            console.log(this.timer);
+        },
+        inputHandler(val, e) {
+            // e.target 是你当前点击的元素
+            // e.currentTarget 是你绑定事件的元素
+            console.log(val, e.target);
+            this.msg = e.target.value;
+        },
+        getValue(val, e) {
+            //如果父组件中不传参，则第一个参数为子组件抛出的参数
+            console.log(val, e);
+            this.msg1 = e;
+        },
+    },
+    created() {},
 };
 </script> 
 
-<style lang='less' scoped>
+<style scoped>
+.aaa {
+  height: 500px;
+}
 </style>
